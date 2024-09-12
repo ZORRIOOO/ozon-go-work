a189883896ba7a281c9ed8dd89d752f2d95d9ddc
Konstantin Mamonov <Konstant.Mamonov@x5.ru>
2024-09-13 00:18:01 +0500

    GOHW-2: Тестирование в Go - Добавил позитивный и негативный тесты для запроса delete-cart

---
+++ cart/internal/pkg/cart/service/delete-cart/handler_test.go
@@ -0,0 +1,38 @@
+package delete_cart
+
+import (
+	"github.com/gojuno/minimock/v3"
+	"github.com/stretchr/testify/require"
+	"homework/cart/internal/pkg/cart/service/delete-cart/mock"
+	"testing"
+)
+
+func TestHandler_DeleteItemsByUser(t *testing.T) {
+	ctrl := minimock.NewController(t)
+	cartRepositoryMock := mock.NewCartRepositoryMock(ctrl)
+
+	addItemHandler := NewHandler(cartRepositoryMock)
+
+	userId := int64(123)
+
+	cartRepositoryMock.DeleteItemsByUserMock.Expect(userId).Return(&userId, nil)
+
+	actualResponse, err := addItemHandler.DeleteItemsByUser(userId)
+
+	expectedResponse := &userId
+	require.Equal(t, expectedResponse, actualResponse)
+	require.NoError(t, err)
+}
+
+func TestHandler_DeleteItemsByUser_WithError(t *testing.T) {
+	ctrl := minimock.NewController(t)
+	cartRepositoryMock := mock.NewCartRepositoryMock(ctrl)
+
+	addItemHandler := NewHandler(cartRepositoryMock)
+
+	userId := int64(0)
+
+	_, err := addItemHandler.DeleteItemsByUser(userId)
+
+	require.Error(t, err)
+}
