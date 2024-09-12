eeac6f5a744b57de90056df2df7f177e7b56078e
Konstantin Mamonov <Konstant.Mamonov@x5.ru>
2024-09-12 21:41:28 +0500

    GOHW-2: Тестирование в Go - Добавил позитивный тест для запроса add-item

---
+++ cart/internal/pkg/cart/service/add-item/handler_test.go
@@ -0,0 +1,66 @@
+package add_item
+
+import (
+	"github.com/gojuno/minimock/v3"
+	"github.com/stretchr/testify/require"
+	"homework/cart/internal/client/api/product/types"
+	"homework/cart/internal/pkg/cart/model"
+	"homework/cart/internal/pkg/cart/service/add-item/mock"
+	"testing"
+)
+
+func TestHandler_AddItem(t *testing.T) {
+	ctrl := minimock.NewController(t)
+	cartRepositoryMock := mock.NewCartRepositoryMock(ctrl)
+	productServiceMock := mock.NewProductServiceMock(ctrl)
+	productToken := "testtoken"
+
+	addItemHandler := NewHandler(cartRepositoryMock, productServiceMock, productToken)
+
+	skuId := int64(1)
+	userId := int64(1)
+	count := uint16(1)
+	cartParams := model.CartParameters{
+		SKU:    skuId,
+		UserId: userId,
+		Count:  count,
+	}
+
+	name := string("Кроссовки 'Nike'")
+	price := uint32(7500)
+	productResponse := types.ProductResponse{
+		Name:  name,
+		Price: price,
+	}
+
+	request := types.ProductRequest{
+		Sku:   skuId,
+		Token: productToken,
+	}
+	productServiceMock.GetProductMock.Expect(request).Return(&productResponse, nil)
+
+	cartItem := model.CartItem{
+		SKU:    skuId,
+		Name:   name,
+		Count:  count,
+		Price:  price,
+		UserId: userId,
+	}
+
+	cartRepositoryMock.AddItemMock.Expect(cartItem).Return(&cartItem, nil)
+	actualResponse, err := addItemHandler.AddItem(cartParams)
+
+	expectedResponse := &model.CartItem{
+		SKU:    skuId,
+		Name:   name,
+		Count:  count,
+		Price:  price,
+		UserId: userId,
+	}
+	require.Equal(t, expectedResponse, actualResponse)
+	require.NoError(t, err)
+
+	productAfterCounter := productServiceMock.GetProductMock.Expect(request).Return(&productResponse, nil).GetProductAfterCounter()
+
+	require.EqualValues(t, productAfterCounter, 1)
+}
