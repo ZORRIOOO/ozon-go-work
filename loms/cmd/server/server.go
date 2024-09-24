55a1df05245db93fa537e298d423b38e5dede6ec
Konstantin Mamonov <Konstant.Mamonov@x5.ru>
2024-09-24 22:50:35 +0500

    GOHW-3: Межсервисное взаимодействие и основы эксплуатации - Доработал CORS для корректных запросов из Swagger UI

--- loms/cmd/server/server.go
+++ loms/cmd/server/server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"github.com/rs/cors"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/health"
@@ -62,9 +63,15 @@ func main() {
 	if err = desc.RegisterLomsHandler(context.Background(), gwmux, conn); err != nil {
 		log.Fatalln("Failed to register gateway:", err.Error())
 	}
+
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowCredentials: true,
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
+	})
 	gwServer := &http.Server{
 		Addr:    httpPort,
-		Handler: mw.HTTPLogging(gwmux),
+		Handler: mw.HTTPLogging(c.Handler(gwmux)),
 	}
 	log.Printf("Serving gRPC-Gateway on PORT: %s\n", gwServer.Addr)
 	log.Fatalln(gwServer.ListenAndServe())
--- loms/go.mod
+++ loms/go.mod
@@ -16,6 +16,7 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rogpeppe/go-internal v1.13.1 // indirect
+	github.com/rs/cors v1.11.1 // indirect
 	golang.org/x/net v0.26.0 // indirect
 	golang.org/x/sys v0.21.0 // indirect
 	golang.org/x/text v0.17.0 // indirect
