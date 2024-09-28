b0ac8e85b17b0da745cbd77cf21e5cd5415f031a
Konstantin Mamonov <Konstant.Mamonov@x5.ru>
2024-09-28 13:09:26 +0500

    GOHW-4: Базы данных - Развернуть Postgres - Подключить Goose - Создать миграции для схем и инициалищации данных - Добавить в Makefile команды для миграций

---
+++ loms/migrations/20240928074910_create_item_table.sql
@@ -0,0 +1,16 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE item
+(
+  id      serial PRIMARY KEY,
+  sku   integer NOT NULL ,
+  count integer NOT NULL,
+  order_id INT NOT NULL REFERENCES "order" (id),
+  unique (order_id)
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE item;
+-- +goose StatementEnd
